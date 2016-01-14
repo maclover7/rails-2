b8e9843edc096c06e2a1e6586f21be5411b98a4b
Matthew Draper <matthew@trebex.net>
2016-01-14 15:55:41 +1030

    Pull the action methods directly onto the adapter
    - Don't execute callbacks on our main listener thread
    - Blanked unsubscribe doesn't appear to be used

--- actioncable/lib/action_cable/storage_adapter/postgres.rb
+++ actioncable/lib/action_cable/storage_adapter/postgres.rb
@@ -3,13 +3,39 @@ require 'thread'
 module ActionCable
   module StorageAdapter
     class Postgres < Base
+<<<<<<< HEAD
+=======
       # The storage instance used for broadcasting. Not intended for direct user use.
-      def broadcast
-        @broadcast ||= PostgresWrapper.new
+>>>>>>> acc6d67... Pull the action methods directly onto the adapter
+      def broadcast(channel, payload)
+        ActiveRecord::Base.connection_pool.with_connection do |ar_conn|
+          pg_conn = ar_conn.raw_connection
+
+          unless pg_conn.is_a?(PG::Connection)
+            raise 'ActiveRecord database must be Postgres in order to use the Postgres ActionCable storage adapter'
+          end
+
+          pg_conn.exec("NOTIFY #{channel}, '#{payload}'")
+        end
       end
 
-      def pubsub
-        PostgresWrapper.new
+<<<<<<< HEAD
+      def subscribe(channel, message_callback, success_callback = nil)
+        Listener.instance.subscribe_to(channel, message_callback, success_callback)
+      end
+
+      def unsubscribe(channel, message_callback)
+        Listener.instance.unsubscribe_to(channel, message_callback)
+=======
+      def subscribe(channel, callback, success_callback = nil)
+        Listener.instance.subscribe_to(channel, callback, success_callback)
+        # Needed for channel/streams.rb#L79
+        ::EM::DefaultDeferrable.new
+      end
+
+      def unsubscribe(channel, callback)
+        Listener.instance.unsubscribe_to(channel, callback)
+>>>>>>> acc6d67... Pull the action methods directly onto the adapter
       end
 
       class Listener
@@ -37,6 +63,7 @@ module ActionCable
                 value = @queue.pop(true)
                 if value.first == :listen
                   pg_conn.exec("LISTEN #{value[1]}")
+                  ::EM.next_tick(&value[2]) if value[2]
                 elsif value.first == :unlisten
                   pg_conn.exec("UNLISTEN #{value[1]}")
                 end
@@ -44,30 +71,26 @@ module ActionCable
 
               pg_conn.wait_for_notify(1) do |chan, pid, message|
                 @subscribers[chan].each do |callback|
-                  callback.call(message)
+                  ::EM.next_tick { callback.call(message) }
                 end
               end
             end
           end
         end
 
-        def subscribe_to(channel, callback)
+        def subscribe_to(channel, callback, success_callback)
           @sync.synchronize do
             if @subscribers[channel].empty?
-              @queue.push([:listen, channel])
+              @queue.push([:listen, channel, success_callback])
             end
 
             @subscribers[channel] << callback
           end
         end
 
-        def unsubscribe_to(channel, callback = nil)
+        def unsubscribe_to(channel, callback)
           @sync.synchronize do
-            if callback
-              @subscribers[channel].delete(callback)
-            else
-              @subscribers.delete(channel)
-            end
+            @subscribers[channel].delete(callback)
 
             if @subscribers[channel].empty?
               @queue.push([:unlisten, channel])
@@ -75,35 +98,6 @@ module ActionCable
           end
         end
       end
-
-      class PostgresWrapper
-        def publish(channel, message)
-          ActiveRecord::Base.connection_pool.with_connection do |ar_conn|
-            pg_conn = ar_conn.raw_connection
-
-            unless pg_conn.is_a?(PG::Connection)
-              raise 'ActiveRecord database must be Postgres in order to use the Postgres ActionCable storage adapter'
-            end
-
-            pg_conn.exec("NOTIFY #{channel}, '#{message}'")
-          end
-        end
-
-        def subscribe(channel, &callback)
-          Listener.instance.subscribe_to(channel, callback)
-          # Needed for channel/streams.rb#L79
-          ::EM::DefaultDeferrable.new
-        end
-
-        def unsubscribe(channel)
-          Listener.instance.unsubscribe_to(channel)
-        end
-
-        def unsubscribe_proc(channel, block)
-          Listener.instance.unsubscribe_to(channel, block)
-        end
-      end
-
     end
   end
 end
--- actioncable/lib/action_cable/storage_adapter/redis.rb
+++ actioncable/lib/action_cable/storage_adapter/redis.rb
@@ -4,26 +4,40 @@ require 'redis'
 module ActionCable
   module StorageAdapter
     class Redis < Base
-      # The redis instance used for broadcasting. Not intended for direct user use.
-      def broadcast
-        @broadcast ||= ::Redis.new(@server.config.config_opts)
+      def broadcast(channel, payload)
+        _broadcast.publish(channel, payload)
       end
 
-      def pubsub
-        redis.pubsub
+      def subscribe(channel, message_callback, success_callback = nil)
+        redis.pubsub.subscribe(channel, &message_callback).tap do |result|
+          result.callback(&success_callback) if success_callback
+        end
+      end
+
+      def unsubscribe(channel, message_callback)
+        redis.pubsub.unsubscribe_proc(channel, message_callback)
       end
 
       private
 
+      # The redis instance used for broadcasting. Not intended for direct user use.
+      def _broadcast
+        @broadcast ||= ::Redis.new(@server.config.config_opts)
+      end
+
       # The EventMachine Redis instance used by the pubsub adapter.
-      def redis
-        @redis ||= EM::Hiredis.connect(@server.config.config_opts[:url]).tap do |redis|
+      def hi_redis_conn
+        @hi_redis_conn ||= EM::Hiredis.connect(@server.config.config_opts[:url]).tap do |redis|
           redis.on(:reconnect_failed) do
             @logger.info "[ActionCable] Redis reconnect failed."
             # logger.info "[ActionCable] Redis reconnected. Closing all the open connections."
             # @connections.map &:close
           end
         end
+      end
+
+      def redis_conn
+        @redis_conn ||= ::Redis.new(@server.config.config_opts)
       end
     end
   end
